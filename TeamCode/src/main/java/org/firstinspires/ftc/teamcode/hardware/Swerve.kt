8bc90dfa92df7c49ebaf3bbd53fa9e3a6ddaa852
gitRaiku <sebi.pintilie@gmail.com>
2023-09-25 14:35:36 +0300

    Work on swerve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Swerve.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Swerve.kt
@@ -1,13 +1,46 @@
 package org.firstinspires.ftc.teamcode.hardware
 
-import org.firstinspires.ftc.teamcode.hardware.CServo
-import org.firstinspires.ftc.teamcode.hardware.Motor
+import android.R.attr
+import org.firstinspires.ftc.teamcode.utils.Util.angDiff
+import org.firstinspires.ftc.teamcode.utils.Util.epsEq
+import kotlin.math.PI
+
 
 class Swerve {
     private val lf = SwerveModule("LF")
     private val lb = SwerveModule("LB")
     private val rf = SwerveModule("RF")
     private val rb = SwerveModule("RB")
+
+    fun turn(turnPower: Double) {
+        lf.angle = PI * 3 / 4
+        lb.angle = PI * 1 / 4
+        rf.angle = PI * 7 / 4
+        rb.angle = PI * 5 / 4
+
+        lf.speed = turnPower
+        lb.speed = turnPower
+        rf.speed = turnPower
+        rb.speed = turnPower
+    }
+
+    fun move(direction: Double, speed: Double, turnPower: Double) {
+        if (epsEq(speed, 0.0)) {
+            turn(turnPower)
+        } else {
+            val turnAngle = turnPower * 45.0
+
+            lf.angle = direction + if (angDiff(direction, PI * 3 / 4) >= PI / 2) turnAngle else -turnAngle
+            lb.angle = direction + if (angDiff(direction, PI * 5 / 4) > PI / 2) turnAngle else -turnAngle
+            rf.angle = direction + if (angDiff(direction, PI * 1 / 4) > PI / 2) turnAngle else -turnAngle
+            rb.angle = direction + if (angDiff(direction, PI * 7 / 4) >= PI / 2) turnAngle else -turnAngle
+
+            lf.speed = speed
+            lb.speed = speed
+            rf.speed = speed
+            rb.speed = speed
+        }
+    }
 
     fun close() {
         lf.close()
